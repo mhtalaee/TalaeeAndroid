79b7b2e38ed37b4946f20b915ebff20a356ef4e9
Talaee <mhtalaee@gmail.com>
2019-02-02 11:33:44 +0330

    Intent Task

--- app/src/main/java/com/example/talaeeandroid/IntentTaskFirstActivity.java
+++ app/src/main/java/com/example/talaeeandroid/IntentTaskFirstActivity.java
@@ -2,7 +2,11 @@ package com.example.talaeeandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class IntentTaskFirstActivity extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class IntentTaskFirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intent_task_first);
+
+        Button btnOk = findViewById(R.id.btnOk);
+        final EditText etName = findViewById(R.id.etName);
+        final EditText etFamily = findViewById(R.id.etFamily);
+        final EditText etAge = findViewById(R.id.etAge);
+        final EditText etEmail = findViewById(R.id.etEmail);
+        final EditText etPhone = findViewById(R.id.etPhone);
+
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(IntentTaskFirstActivity.this, IntentTaskSecondActivity.class);
+                intent.putExtra(getString(R.string.lName), etName.getText().toString());
+                intent.putExtra(getString(R.string.lFamily), etFamily.getText().toString());
+                intent.putExtra(getString(R.string.lAge), etAge.getText().toString());
+                intent.putExtra(getString(R.string.lEmail), etEmail.getText().toString());
+                intent.putExtra(getString(R.string.lNumber), etPhone.getText().toString());
+                startActivity(intent);
+            }
+        });
     }
 }
