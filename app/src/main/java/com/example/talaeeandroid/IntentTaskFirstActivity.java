a445c90844cb6e7e4391a27b30059aa162098227
Talaee <mhtalaee@gmail.com>
2019-02-02 08:51:54 +0330

    Intent task

--- app/src/main/java/com/example/talaeeandroid/IntentTaskFirstActivity.java
+++ app/src/main/java/com/example/talaeeandroid/IntentTaskFirstActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class FirstIntentTaskActivity extends AppCompatActivity {
+public class IntentTaskFirstActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_first_intent_task);
+        setContentView(R.layout.activity_intent_task_first);
     }
 }
