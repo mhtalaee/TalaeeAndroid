51f5401fa1e3b17571fb55c1ee954419eb31e1fd
Talaee <mhtalaee@gmail.com>
2019-02-04 15:45:03 +0330

    SharedPreferences

--- app/src/main/java/com/example/talaeeandroid/IntentTaskFirstActivity.java
+++ app/src/main/java/com/example/talaeeandroid/IntentTaskFirstActivity.java
@@ -2,13 +2,18 @@ package com.example.talaeeandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.example.talaeeandroid.content.Constant;
 
 public class IntentTaskFirstActivity extends AppCompatActivity {
 
@@ -19,30 +24,57 @@ public class IntentTaskFirstActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intent_task_first);
 
-        Button btnOk = findViewById(R.id.btnOk);
         final EditText etName = findViewById(R.id.etName);
         final EditText etFamily = findViewById(R.id.etFamily);
         final EditText etAge = findViewById(R.id.etAge);
         final EditText etEmail = findViewById(R.id.etEmail);
         final EditText etPhone = findViewById(R.id.etPhone);
+        final AutoCompleteTextView etCountry = findViewById(R.id.etCountry);
+        Button btnOk = findViewById(R.id.btnOk);
+        Button btnCancel = findViewById(R.id.btnCancel);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, COUNTRIES);
+        AutoCompleteTextView textView = findViewById(R.id.etCountry);
+        textView.setAdapter(adapter);
 
         btnOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(IntentTaskFirstActivity.this, IntentTaskSecondActivity.class);
-                intent.putExtra(getString(R.string.lName), etName.getText().toString());
-                intent.putExtra(getString(R.string.lFamily), etFamily.getText().toString());
-                intent.putExtra(getString(R.string.lAge), etAge.getText().toString());
-                intent.putExtra(getString(R.string.lEmail), etEmail.getText().toString());
-                intent.putExtra(getString(R.string.lNumber), etPhone.getText().toString());
-                startActivity(intent);
+//                Intent intent = new Intent(IntentTaskFirstActivity.this, IntentTaskSecondActivity.class);
+//                intent.putExtra(Constant.EXTRA_COUNTRY, etCountry.getText().toString());
+//                intent.putExtra(Constant.EXTRA_NAME, etName.getText().toString());
+//                intent.putExtra(Constant.EXTRA_FAMILY, etFamily.getText().toString());
+//                intent.putExtra(Constant.EXTRA_AGE, etAge.getText().toString());
+//                intent.putExtra(Constant.EXTRA_EMAIL, etEmail.getText().toString());
+//                intent.putExtra(Constant.EXTRA_PHONE, etPhone.getText().toString());
+//                startActivity(intent);
+
+                SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(IntentTaskFirstActivity.this);
+                sp.edit().putString(Constant.COUNTRY, etCountry.getText().toString()).apply();
+                sp.edit().putString(Constant.NAME, etName.getText().toString()).apply();
+                sp.edit().putString(Constant.FAMILY, etFamily.getText().toString()).apply();
+                sp.edit().putString(Constant.AGE, etAge.getText().toString()).apply();
+                sp.edit().putString(Constant.EMAIL, etEmail.getText().toString()).apply();
+                sp.edit().putString(Constant.PHONE, etPhone.getText().toString()).apply();
+
+                Intent returnIntent = new Intent();
+                returnIntent.putExtra(Constant.NAME, etName.getText().toString());
+                setResult(Activity.RESULT_OK, returnIntent);
+                finish();
+
             }
         });
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, COUNTRIES);
-        AutoCompleteTextView textView = findViewById(R.id.countries_list);
-        textView.setAdapter(adapter);
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent returnIntent = new Intent();
+                setResult(Activity.RESULT_CANCELED, returnIntent);
+                finish();
+            }
+        });
+
 
     }
 }
