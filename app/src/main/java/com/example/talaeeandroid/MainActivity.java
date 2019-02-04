6e00e81a02ec506a13bdd87013ea610363401de4
Talaee <mhtalaee@gmail.com>
2019-02-04 15:46:19 +0330

    SharedPreferences

--- app/src/main/java/com/example/talaeeandroid/MainActivity.java
+++ app/src/main/java/com/example/talaeeandroid/MainActivity.java
@@ -7,49 +7,9 @@ import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
-    short counter = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("LifeCycle" + counter++, "onCreaet");
-    }
-
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d("LifeCycle" + counter++, "onStart");
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d("LifeCycle" + counter++, "onResume");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d("LifeCycle" + counter++, "onPause");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d("LifeCycle" + counter++, "onStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d("LifeCycle" + counter++, "onDestroy");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d("LifeCycle" + counter++, "onRestart");
     }
 }
