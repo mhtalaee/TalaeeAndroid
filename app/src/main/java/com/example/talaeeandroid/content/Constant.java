fbe5cb4e3122844a87f4d52151c2b394b48547b3
Talaee <mhtalaee@gmail.com>
2019-02-04 15:43:39 +0330

    SharedPreferences

---
+++ app/src/main/java/com/example/talaeeandroid/content/Constant.java
@@ -0,0 +1,12 @@
+package com.example.talaeeandroid.content;
+
+public class Constant {
+
+    public static final String COUNTRY = "COUNTRY";
+    public static final String NAME = "NAME";
+    public static final String FAMILY = "FAMILY";
+    public static final String AGE = "AGE";
+    public static final String EMAIL = "EMAIL";
+    public static final String PHONE = "PHONE";
+    public static final int EDIT_ACTIVITY_REQUEST_CODE = 100;
+}
