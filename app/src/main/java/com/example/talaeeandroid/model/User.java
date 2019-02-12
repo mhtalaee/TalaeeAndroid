2e14abc1971c6a5d64f4691426988169e0ce6c3b
Talaee <mhtalaee@gmail.com>
2019-02-12 15:54:07 +0330

    RecyclerView

---
+++ app/src/main/java/com/example/talaeeandroid/model/User.java
@@ -0,0 +1,60 @@
+package com.example.talaeeandroid.model;
+
+public class User {
+
+    private String country;
+    private String name;
+    private String family;
+    private int age;
+    private String email;
+    private String phone;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
