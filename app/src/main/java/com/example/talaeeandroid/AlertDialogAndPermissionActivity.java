f277a2416986b2ceef859aaf27280e70c5c3bb59
mhtalaee <mhtalaee@gmail.com>
2019-02-18 22:06:12 +0330

    AlertDialogAndPermission

--- app/src/main/java/com/example/talaeeandroid/AlertDialogAndPermissionActivity.java
+++ app/src/main/java/com/example/talaeeandroid/AlertDialogAndPermissionActivity.java
@@ -2,11 +2,17 @@ package com.example.talaeeandroid;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class AlertDialogAndPermissionActivity extends AppCompatActivity {
 
@@ -22,28 +28,36 @@ public class AlertDialogAndPermissionActivity extends AppCompatActivity {
         btnOpenCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(AlertDialogAndPermissionActivity.this);
 
-                alertDialogBuilder.setTitle("Open Camera");
-                alertDialogBuilder.setMessage("Allow app to access your camera?");
-                alertDialogBuilder.setIcon(R.drawable.camera);
-                alertDialogBuilder.setCancelable(Boolean.FALSE);
-                alertDialogBuilder.setPositiveButton(getString(R.string.lYes), new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
+                if (ContextCompat.checkSelfPermission(AlertDialogAndPermissionActivity.this, Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED) {
 
-                    }
-                });
+                    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(AlertDialogAndPermissionActivity.this);
+                    alertDialogBuilder.setTitle("Open Camera");
+                    alertDialogBuilder.setMessage("Allow app to access your camera?");
+                    alertDialogBuilder.setIcon(R.drawable.camera);
+                    alertDialogBuilder.setCancelable(Boolean.FALSE);
+                    alertDialogBuilder.setPositiveButton(getString(R.string.lYes), new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(AlertDialogAndPermissionActivity.this, new String[]{Manifest.permission.CAMERA},
+                                    100);
+                        }
+                    });
 
-                alertDialogBuilder.setNegativeButton(R.string.lNo, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
+                    alertDialogBuilder.setNegativeButton(R.string.lNo, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Toast.makeText(AlertDialogAndPermissionActivity.this, "User denied camera permission", Toast.LENGTH_LONG).show();
+                        }
+                    });
 
-                    }
-                });
+                    AlertDialog dialog = alertDialogBuilder.create();
+                    dialog.show();
 
-                AlertDialog dialog = alertDialogBuilder.create();
-                dialog.show();
+                } else {
+                    Toast.makeText(AlertDialogAndPermissionActivity.this, "Camer permission allready granted", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
